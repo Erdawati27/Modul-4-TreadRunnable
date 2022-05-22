c4a681b5603d5b13ee67ab699314e05737601790
erdawati <erdamly1@gmail.com>
2022-05-22 22:43:42 +0700

    Implementasi Thread dan Runnable (Simple Thread)

---
+++ app/src/main/java/com/example/threadrunnable/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.threadrunnable
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+    lateinit var mhandler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mhandler = object : Handler() {
+            override fun handleMessage(msg: Message) {
+                textView.text = msg?.data?.getString("counter")
+            }
+        }
+        button.setOnClickListener {
+            Thread(Runnable {
+                killSomeTime()
+            }).start()
+        }
+    }
+    private fun killSomeTime() {
+        for (i in 1..20) {
+            var msg = Message.obtain()
+            msg.data.putString("counter", i.toString())
+            mhandler.sendMessage(msg)
+            Thread.sleep(2000)
+        }
+    }
+}
